abf08223332f381432c9776ae71e66b58f56b7a0
Wouter Kursten <wouter@retouw.nl>
2023-08-21 20:16:01 +0200

    21082023

--- horizon_functions.py
+++ horizon_functions.py
@@ -85,4 +85,127 @@ class Connection:
                 raise Exception("Error: " + str(e))
             else:
                 return response.status_code
-            
+
+class Federation:
+    def __init__(self, url: str, access_token: dict):
+        """Default object for the pools class where all Desktop Pool Actions will be performed."""
+        self.url = url
+        self.access_token = access_token
+
+    def get_cloud_pod_federation(self) -> dict:
+        """Retrieves the pod federation details.
+
+        Available for Horizon 8 2012 and later."""
+        response = requests.get(f'{self.url}/rest/federation/v1/cpa', verify=False,  headers=self.access_token)
+        if response.status_code == 400:
+            error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        if response.status_code == 404:
+            error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        elif response.status_code == 403:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        elif response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise "Error: " + str(e)
+            else:
+                return response.json()
+
+    def get_pods(self) -> list:
+        """Lists all the pods in the pod federation.
+
+        Available for Horizon 8 2012 and later."""
+        response = requests.get(f'{self.url}/rest/federation/v1/pods', verify=False,  headers=self.access_token)
+        if response.status_code == 400:
+            if "error_messages" in response.json():
+                error_message = (response.json())["error_messages"]
+            else:
+                error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        if response.status_code == 404:
+            error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        elif response.status_code == 403:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        elif response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise "Error: " + str(e)
+            else:
+                return response.json()
+
+    def get_pod(self, pod_id:str) -> dict:
+        """Retrieves a given pod from the pod federation.
+
+        Requires pod_id as a string
+        Available for Horizon 8 2012 and later."""
+        response = requests.get(f'{self.url}/rest/federation/v1/pods/{pod_id}', verify=False,  headers=self.access_token)
+        if response.status_code == 400:
+            if "error_messages" in response.json():
+                error_message = (response.json())["error_messages"]
+            else:
+                error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        if response.status_code == 404:
+            error_message = (response.json())["error_message"]
+            raise Exception(f"Error {response.status_code}: {error_message}")
+        elif response.status_code == 403:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        elif response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise "Error: " + str(e)
+            else:
+                return response.json()
+
+class Monitor:
+    def __init__(self, url: str, access_token: dict):
+        """Default object for the monitor class used for the monitoring of the various VMware Horiozn services."""
+        self.url = url
+        self.access_token = access_token
+
+    def connection_servers(self) -> list:
+        """Lists monitoring information related to Connection Servers of the environment.
+
+        Available for Horizon 7.10 and later."""
+        response = requests.get(f'{self.url}/rest/monitor/v2/connection-servers', verify=False,  headers=self.access_token)
+        if response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise "Error: " + str(e)
+            else:
+                return response.json()
+
+class Config:
+    def __init__(self, url: str, access_token: dict):
+        """Default object for the config class used for the general configuration of VMware Horizon."""
+        self.url = url
+        self.access_token = access_token
+        
+    def get_environment_properties(self) -> dict:
+        """Retrieves the environment settings.
+
+        Available for Horizon 7.12 and later."""
+        response = requests.get(f'{self.url}/rest/config/v2/environment-properties', verify=False,  headers=self.access_token)
+        if response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.reason}")
+        else:
+            try:
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                raise "Error: " + str(e)
+            else:
+                return response.json()
